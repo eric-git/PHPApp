09f6759d9570646ace2ecc9fad5d3ffad4aafc04
Eric Wu <wu_yuqing@hotmail.com>
2022-09-04 20:32:02 +1000

    updated phpinfo page & extrenal urls

--- Shared/Footer.php
+++ Shared/Footer.php
@@ -1,12 +1,12 @@
 <footer id="footer">
     <ul class="icons">
-        <li><a href="https://twitter.com/usigovau?lang=en" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-        <li><a href="https://www.facebook.com/USIgovau" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-        <li><a href="https://au.linkedin.com/company/usioffice" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-        <li><a href="https://www.instagram.com/uniquestudentidentifier" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-        <li><a href="https://www.youtube.com/channel/UCa3TZhpgi6PJmFkHPwZs6sQ" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
-        <li><a href="https://github.com/bartland-usi" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-        <li><a href="https://www.usi.gov.au/contact-us" class="icon solid fa-address-book"><span class="label">Contact</span></a></li>
+        <li><a href="https://twitter.com/usigovau?lang=en" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+        <li><a href="https://www.facebook.com/USIgovau" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
+        <li><a href="https://au.linkedin.com/company/usioffice" class="icon brands fa-linkedin-in" target="_blank"><span class="label">LinkedIn</span></a></li>
+        <li><a href="https://www.instagram.com/uniquestudentidentifier" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+        <li><a href="https://www.youtube.com/channel/UCa3TZhpgi6PJmFkHPwZs6sQ" class="icon brands fa-youtube" target="_blank"><span class="label">Youtube</span></a></li>
+        <li><a href="https://github.com/bartland-usi" class="icon brands fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+        <li><a href="https://www.usi.gov.au/contact-us" class="icon solid fa-address-book" target="_blank"><span class="label">Contact</span></a></li>
     </ul>
     <ul class="copyright">
         <li>USI 2022</li>
