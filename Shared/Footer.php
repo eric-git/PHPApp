99689ec008eb434a89dafa93b65416b876f3f4c7
Eric Wu <wu_yuqing@hotmail.com>
2023-10-17 11:42:49 +1100

    Features fix minor issues (#14)
    * added error handling, url rewrite for routing
    
    * updated phpinfo page & extrenal urls
    
    * updated environments as ATO confihured new RP, starting Linux setup
    
    * add sample php.ini
    
    * update ato sts ep
    
    * update key store
    
    ---------
    
    Co-authored-by: Eric Wu <Eric.Wu@dewr.gov.au>

--- Shared/Footer.php
+++ Shared/Footer.php
@@ -1,12 +1,12 @@
 <footer id="footer">
     <ul class="icons">
-        <li><a href="https://twitter.com/usigovau?lang=en" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
-        <li><a href="https://www.facebook.com/USIgovau" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-        <li><a href="https://au.linkedin.com/company/usioffice" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-        <li><a href="https://www.instagram.com/uniquestudentidentifier" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-        <li><a href="https://www.youtube.com/channel/UCa3TZhpgi6PJmFkHPwZs6sQ" class="icon brands alt fa-youtube"><span class="label">Youtube</span></a></li>
-        <li><a href="https://github.com/bartland-usi" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-        <li><a href="https://www.usi.gov.au/contact-us" class="icon solid alt fa-address-book"><span class="label">Contact</span></a></li>
+        <li><a href="https://twitter.com/usigovau?lang=en" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+        <li><a href="https://www.facebook.com/USIgovau" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
+        <li><a href="https://au.linkedin.com/company/usioffice" class="icon brands fa-linkedin-in" target="_blank"><span class="label">LinkedIn</span></a></li>
+        <li><a href="https://www.instagram.com/uniquestudentidentifier" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+        <li><a href="https://www.youtube.com/channel/UCa3TZhpgi6PJmFkHPwZs6sQ" class="icon brands fa-youtube" target="_blank"><span class="label">Youtube</span></a></li>
+        <li><a href="https://github.com/bartland-usi" class="icon brands fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+        <li><a href="https://www.usi.gov.au/contact-us" class="icon solid fa-address-book" target="_blank"><span class="label">Contact</span></a></li>
     </ul>
     <ul class="copyright">
         <li>USI 2022</li>
